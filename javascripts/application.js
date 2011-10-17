ed370cd4186681d7a673d8134900f86eeef85dee
domcerminara <dompkt2013@gmail.com>
2011-10-17 00:19:29 -0400

    Added Twitter, css work

--- javascripts/application.js
+++ javascripts/application.js
@@ -7,7 +7,7 @@ $(function() {
       data: {
         method: 'artist.getSimilar',
         autocorrect: '1', //Turns on autocorrecting
-        limit: '1', //Gets top result
+        limit: '3', //Gets top 3 results
         artist: $("#artist").val(),
         format: 'json',
         api_key: '028a648a94b5ca3a256ed241d7952a20',
@@ -22,8 +22,6 @@ $(function() {
 
 //Adds result to HTML
 function displayResult(result) {
-   console.log(result);
-   var name = result.similarartists.artist.name; //Name of similar artist
-   $("#artistinfo").html("<p> Since you like " + $("#artist").val() + ", you should check out <a id='show' href='http://"+ result.similarartists.artist.url +"'>" + name + "</a> too!</p>");
+   $("#artistinfo").html("<p> Since you like " + $("#artist").val() + ", you should check out <a href='http://"+ result.similarartists.artist[0].url + "' target='_blank'>" + result.similarartists.artist[0].name + "</a>, <a href='http://"+ result.similarartists.artist[1].url + "' target='_blank'>" + result.similarartists.artist[1].name + "</a>, and <a href='http://"+ result.similarartists.artist[2].url + "' target='_blank'>" + result.similarartists.artist[2].name + "</a> too!</p>");
 };
 
